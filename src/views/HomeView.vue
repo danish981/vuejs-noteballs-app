b90aeda78df691e36b6b33acfec3cae3a342f165
danish <danishmehmood981@gmail.com>
2024-12-03 03:57:48 +0500

    So simple counter application created

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,41 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div class="home">
+    <div>
+      <button @click="decreaseCounter" class="btn">-</button>
+      <span class="counter"> {{ counter }} </span>
+      <button @click="increaseCounter" class="btn">+</button>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++;
+    },
+    decreaseCounter() {
+      this.counter--;
+    }
+  }
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+.btn, .counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
